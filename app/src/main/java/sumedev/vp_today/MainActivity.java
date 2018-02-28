ba4fa4daabe129850d5b2fefc1d8354739a5f6dc
Melvin Zaehl <melverinzahl@gmail.com>
2018-02-28 22:29:45 +0100

    ++Better HTML View ++Reformat --Unnötige Buttons

--- app/src/main/java/sumedev/vp_today/MainActivity.java
+++ app/src/main/java/sumedev/vp_today/MainActivity.java
@@ -1,15 +1,15 @@
 package sumedev.vp_today;
 
-import android.content.Context;
-import android.content.Intent;
-import android.security.NetworkSecurityPolicy;
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,24 +19,39 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    TextView txt;
-    String tmp = "";
+    private DatePickerDialog.OnDateSetListener mDateSetListener;
+    private String date;
+    private TextView textView;
+    private String tmp = "";
+    private Thread t;
 
-    Thread t;
+    //Temporäre Anzeige des HTML Quelltextes der abgerufenden Seite -> Endeffekt wirds eine Listview werden
+    EditText txt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //View init
+        txt = findViewById(R.id.text);
+        textView = findViewById(R.id.textView);
+
+        Button btnDate = findViewById(R.id.btnDate);
+
+
+        //Threads
         t = new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    String urlS = "https://vp.gymnasium-odenthal.de/god/2018-03-02";
+                    String urlS = "https://vp.gymnasium-odenthal.de/god/" + date;
+                    Log.e("LOG", urlS);
                     String authStringEnc = "dnA6Z29kOTIwMQ==";
                     URL url = new URL(urlS);
                     URLConnection urlConnection = url.openConnection();
@@ -61,30 +76,43 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        Button btnUpdate = findViewById(R.id.btnUpdate);
-        btnUpdate.setOnClickListener(new View.OnClickListener() {
+
+        //Listeners
+        btnDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int year = cal.get(Calendar.YEAR);
+                int month = cal.get(Calendar.MONTH);
+                int day = cal.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(
+                        MainActivity.this,
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        mDateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+
+                if(day >= 10 && month >= 10)
+                    date = year + "-" + month + "-" + day;
+                else if(day < 10)
+                    date = year + "-" + month + "-0" + day;
+                else
+                    date = year + "-0" + month + "-" + day;
+                textView.setText("Deine Vertretungen für : " + day + "." + month + "." + year);
                 update();
             }
-        });
-
-       txt = findViewById(R.id.text);
-
-
-        Button btnKurse = findViewById(R.id.btnKurse);
-        btnKurse.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.e("[LOG]", "Kurse clicked");
-            }
-        });
-
-        //Last
-        update();
+        };
     }
 
-    private void update(){
+    private void update() {
         t.start();
         try {
             t.join();
@@ -95,22 +123,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menuheader, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch ( item.getItemId() ) {
-            case R.id.settings:
-                Intent myIntent = new Intent(getBaseContext(), SettingsActivity.class);
-                this.startActivity(myIntent);
-                break;
-        }
-
-        return true;
-    }
 }
