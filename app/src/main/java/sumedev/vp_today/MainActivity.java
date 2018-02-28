502383932a873335ec113888c6b02d66a6a21955
Melvin Zaehl <melverinzahl@gmail.com>
2018-02-28 20:24:54 +0100

    Added Button Click Listener

--- app/src/main/java/sumedev/vp_today/MainActivity.java
+++ app/src/main/java/sumedev/vp_today/MainActivity.java
@@ -2,6 +2,9 @@ package sumedev.vp_today;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                update();
+            }
+        });
+
+        Button btnKurse = findViewById(R.id.btnKurse);
+        btnKurse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("[LOG]", "Kurse clicked");
+            }
+        });
+    }
+
+    private void update(){
+        Log.e("[LOG]", "Update Clicked");
     }
 }
